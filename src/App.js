c9b3f3d4f938e5a7d4244692a25f1c1444b48884
Cee <cee@Cees-MacBook-Pro.local>
2019-09-21 22:59:42 +0700

    config tailwind

--- src/App.js
+++ src/App.js
@@ -1,6 +1,11 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+import 'antd/dist/antd.css';
+import './index.css';
+import { Layout, Menu, Icon } from 'antd';
+
+const { Header, Sider, Content } = Layout;
 
 function App() {
   return (
@@ -10,6 +15,7 @@ function App() {
         <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>
+        <div className='text-teal-400'>tailwind</div>
         <a
           className="App-link"
           href="https://reactjs.org"
